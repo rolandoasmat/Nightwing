f59ca5dddb04b943d0b38e9e1088cdc07b1ce6b2
Rolando Asmat <vhesener@visa.com>
2017-05-17 01:33:26 -0500

    removed todo

--- app/src/main/java/com/asmat/rolando/popularmovies/models/RequestType.java
+++ app/src/main/java/com/asmat/rolando/popularmovies/models/RequestType.java
@@ -4,7 +4,7 @@ package com.asmat.rolando.popularmovies.models;
  * Created by rolandoasmat on 2/11/17.
  */
 
-public class RequestType { // TODO remove! no enums in here
+public class RequestType {
     public static final int POPULAR = 0;
     public static final int TOP_RATED = 1;
 }
