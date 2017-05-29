5d063ec13a26f6a1c4e0fca3ad0dd65908a37eb0
Rolando Asmat <rolandoasmat@gmail.com>
2017-05-29 09:57:04 -0500

    gitignore

--- app/src/main/java/com/asmat/rolando/popularmovies/models/MovieAdapterOnClickHandler.java
+++
@@ -1,9 +0,0 @@
-package com.asmat.rolando.popularmovies.models;
-
-/**
- * Created by rolandoasmat on 2/12/17.
- */
-
-public interface MovieAdapterOnClickHandler {
-    void onClick(Movie movie);
-}
--- app/src/main/java/com/asmat/rolando/popularmovies/models/TrailerAdapterOnClickHandler.java
+++
@@ -1,9 +0,0 @@
-package com.asmat.rolando.popularmovies.models;
-
-/**
- * Created by rolandoasmat on 5/20/17.
- */
-
-public interface TrailerAdapterOnClickHandler {
-    void onClick(Video trailer);
-}
