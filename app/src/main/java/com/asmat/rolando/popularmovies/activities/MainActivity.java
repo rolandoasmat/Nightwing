f3a61fe80565dc00722e32336525ec89a316a330
Rolando Asmat <rolandoasmat@gmail.com>
2017-06-01 23:03:31 -0500

    Cleanup

--- app/src/main/java/com/asmat/rolando/popularmovies/activities/MainActivity.java
+++ app/src/main/java/com/asmat/rolando/popularmovies/activities/MainActivity.java
@@ -1,7 +1,5 @@
 package com.asmat.rolando.popularmovies.activities;
 
-import android.content.Context;
-import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -9,15 +7,14 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import com.asmat.rolando.popularmovies.R;
 import com.asmat.rolando.popularmovies.adapters.SectionsPagerAdapter;
-import com.asmat.rolando.popularmovies.models.Movie;
-import com.asmat.rolando.popularmovies.models.MovieAdapterOnClickHandler;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static String TAG = MainActivity.class.getSimpleName();
     private SectionsPagerAdapter mSectionsPagerAdapter;
-    private ViewPager mViewPager;
+    @BindView(R.id.container) ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(), this);
-        mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
