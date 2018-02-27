4326c3c84f2fa0386e29ae0b41bb5c88bcbd3ddf
Rolando Asmat <rolandoasmat@gmail.com>
2018-02-27 08:58:42 -0600

    Progress on switching to Search Widget instead of search dialog

--- app/src/main/java/com/asmat/rolando/popularmovies/activities/MainActivity.java
+++ app/src/main/java/com/asmat/rolando/popularmovies/activities/MainActivity.java
@@ -1,11 +1,17 @@
 package com.asmat.rolando.popularmovies.activities;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+
 import com.asmat.rolando.popularmovies.R;
 import com.asmat.rolando.popularmovies.adapters.SectionsPagerAdapter;
 
@@ -34,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
             Log.v(TAG, "got page: "+page+" from bundle.");
             mViewPager.setCurrentItem(page);
         }
+        onSearchRequested();
     }
 
     @Override
@@ -58,6 +65,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         Log.v(TAG, "onPause");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the options menu from XML
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        // Assumes current activity is the searchable activity
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
+
+        return true;
     }
 
     @Override
