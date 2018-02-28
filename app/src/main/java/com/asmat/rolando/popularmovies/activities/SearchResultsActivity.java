53884c5e34f4cb9573b113f540e6b9cef538fc17
Rolando Asmat <rolandoasmat@gmail.com>
2018-02-28 07:11:19 -0600

    cleanup. started adding movie fragment to search results activity

--- app/src/main/java/com/asmat/rolando/popularmovies/activities/SearchResultsActivity.java
+++ app/src/main/java/com/asmat/rolando/popularmovies/activities/SearchResultsActivity.java
@@ -4,36 +4,30 @@ import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.View;
 import android.widget.Toast;
 
 import com.asmat.rolando.popularmovies.R;
+import com.asmat.rolando.popularmovies.fragments.MovieGridFragment;
 
 public class SearchResultsActivity extends AppCompatActivity {
+    MovieGridFragment mResultsGrid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // TODO need to rething the grid fragment
+        mResultsGrid = (MovieGridFragment) getSupportFragmentManager().findFragmentById(R.id.results_grid);
+
 
         // Get the intent, verify the action and get the query
         Intent intent = getIntent();
--- app/src/main/java/com/asmat/rolando/popularmovies/fragments/MovieGridFragment.java
+++ app/src/main/java/com/asmat/rolando/popularmovies/fragments/MovieGridFragment.java
@@ -41,29 +41,20 @@ public class MovieGridFragment extends Fragment implements MovieAdapterOnClickHa
     private RecyclerView mMoviesGrid;
     private LinearLayout mNoInternetView;
     private boolean fetchingMovies = false;
-    private final String TAG = "RA:MovieGridFragment:";
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        Log.v(tag(), "onAttach");
         mContext = context;
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.v(tag(), "onCreate");
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        Log.v(tag(), "onCreateView");
         View rootView = inflater.inflate(R.layout.fragment_movie_grid, container, false);
-        mMoviesGrid = (RecyclerView) rootView.findViewById(R.id.rv_movie_grid);
-        mNoInternetView = (LinearLayout) rootView.findViewById(R.id.no_internet_layout);
+        mMoviesGrid = rootView.findViewById(R.id.rv_movie_grid);
+        mNoInternetView = rootView.findViewById(R.id.no_internet_layout);
         GridLayoutManager mMoviesGridLayoutManager;
         if(page == 1) {
             int numOfCol = ViewUtils.calculateNumberOfColumns(mContext);
@@ -85,21 +76,8 @@ public class MovieGridFragment extends Fragment implements MovieAdapterOnClickHa
     }
 
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        Log.v(tag(), "onActivityCreated");
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        Log.v(tag(), "onStart");
-    }
-
-    @Override
     public void onResume() {
         super.onResume();
-        Log.v(tag(), "onResume");
         if(!NetworkUtils.isOnline(getContext())) {
             // User has no internet
             mMoviesGrid.setVisibility(View.GONE);
@@ -111,47 +89,12 @@ public class MovieGridFragment extends Fragment implements MovieAdapterOnClickHa
         }
     }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        Log.v(tag(), "onPause");
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        Log.v(tag(), "onStop");
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        Log.v(tag(), "onDestroyView");
-    }
-
-    // DOES NOT get called as user swipes right/left on view pager
-    // DOES get called on screen rotation
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Log.v(tag(), "onDestroy");
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        Log.v(tag(), "onDetach");
-    }
-
     public MovieGridFragment() {
         page = 1;
-        Log.v(tag(), "MovieGridFragment constructor.");
     }
 
     public void setTypeOfMovies(int typeOfMovies) {
         this.typeOfMovies = typeOfMovies;
-        Log.v(tag(), "setTypeOfMovies");
-        Log.v(tag(), "typeOfMovies: "+typeOfMovies);
     }
 
     private RecyclerView.OnScrollListener createScrollListener(final GridLayoutManager layoutManager) {
@@ -161,9 +104,6 @@ public class MovieGridFragment extends Fragment implements MovieAdapterOnClickHa
                 if(dy > 0 && !fetchingMovies) { // User is scrolling down
                     int positionOfLastItem = layoutManager.getItemCount()-1;
                     int currentPositionOfLastVisibleItem = layoutManager.findLastVisibleItemPosition();
-                    Log.v(tag(), "onScrolled");
-                    Log.v(tag(), "positionOfLastItem: "+positionOfLastItem);
-                    Log.v(tag(), "currentPositionOfLastVisibleItem: "+currentPositionOfLastVisibleItem);
                     if(currentPositionOfLastVisibleItem >= positionOfLastItem - 5){
                         fetchMovies();
                     }
@@ -173,20 +113,13 @@ public class MovieGridFragment extends Fragment implements MovieAdapterOnClickHa
     }
 
     private void fetchMovies() {
-        Log.v(tag(), "fetchMovies");
         fetchingMovies = true;
         LoaderManager loaderManager = getActivity().getSupportLoaderManager();
         loaderManager.restartLoader(typeOfMovies, null, fetchMoviesCallbacks);
     }
 
     @Override
-    public void onSaveInstanceState(Bundle outState) {
-        Log.v(tag(), "onSaveInstanceState");
-    }
-
-    @Override
     public void onClick(Movie movie) {
-        Log.v(tag(), "onClick");
         Class destinationClass = MovieDetailActivity.class;
         Intent intentToStartDetailActivity = new Intent(mContext, destinationClass);
         intentToStartDetailActivity.putExtra(MovieDetailActivity.INTENT_EXTRA_TAG, movie);
@@ -194,8 +127,6 @@ public class MovieGridFragment extends Fragment implements MovieAdapterOnClickHa
     }
 
     private void setFetchMoviesLoaderCallback() {
-        Log.v(tag(), "setFetchMoviesLoaderCallback");
-
         fetchMoviesCallbacks = new LoaderManager.LoaderCallbacks<ArrayList<Movie>>() {
 
             @Override
@@ -204,16 +135,12 @@ public class MovieGridFragment extends Fragment implements MovieAdapterOnClickHa
 
                     @Override
                     protected void onStartLoading() {
-                        Log.v(tag(), "onStartLoading");
                         forceLoad();
                     }
 
                     @Override
                     public ArrayList<Movie> loadInBackground() {
-                        Log.v(tag(), "loadInBackground");
                         try {
-                            Log.v(tag(), "typeOfMovies: "+typeOfMovies);
-                            Log.v(tag(), "page: "+page);
                             return MovieApiManager.fetchMoviesOfType(typeOfMovies, page);
                         } catch (Exception e) {
                             e.printStackTrace();
@@ -225,19 +152,15 @@ public class MovieGridFragment extends Fragment implements MovieAdapterOnClickHa
 
             @Override
             public void onLoadFinished(Loader<ArrayList<Movie>> loader, ArrayList<Movie> data) {
-                Log.v(tag(), "onLoadFinished");
                 // TODO hide loader
                 if(data == null) {
                     // TODO show error message
                 } else {
                     if(page == 1) {
-                        Log.v(tag(), "setting movies");
                         mMoviesGridAdapter.setMovies(data);
                     } else {
-                        Log.v(tag(), "adding movies");
                         mMoviesGridAdapter.addMovies(data);
                     }
-                    Log.v(tag(), "data: "+data.toString());
                     page++;
                     fetchingMovies = false;
                 }
@@ -246,16 +169,9 @@ public class MovieGridFragment extends Fragment implements MovieAdapterOnClickHa
 
             @Override
             public void onLoaderReset(Loader<ArrayList<Movie>> loader) {
-                Log.v(tag(), "onLoaderReset");
             }
         };
     }
 
-    private String tag() {
-        return TAG+typeOfMovies;
-    }
 
-    public void retry(View view) {
-        Log.v(TAG, "retry pressed");
-    }
 }
