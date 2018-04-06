c33b7ea43489ed555ff9b34cab036d0df35f5b38
Rolando Asmat <rolandoasmat@gmail.com>
2018-04-06 09:51:15 -0500

    Added unit tests for Video class

--- app/src/test/java/com/asmat/rolando/popularmovies/models/MovieTest.java
+++ app/src/test/java/com/asmat/rolando/popularmovies/models/MovieTest.java
@@ -1,13 +1,25 @@
 package com.asmat.rolando.popularmovies.models;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MovieTest {
+    Movie movie;
+
+    @Before
+    public void setUp() {
+        movie = getTestObject();
+    }
+
+    @After
+    public void tearDown() {
+        movie = null;
+    }
 
     @Test
     public void getReleaseDateFormatted() {
-        Movie movie = getMovieTestObject();
         String actual = movie.getReleaseDateFormatted();
         String expected = "March 28, 2018";
         assertEquals(expected, actual);
@@ -15,7 +27,6 @@ public class MovieTest {
 
     @Test
     public void getReleaseDateFormattedInvalidDate() {
-        Movie movie = getMovieTestObject();
         movie.setReleaseDate("not parcelable date string");
         String actual = movie.getReleaseDateFormatted();
         String expected = "Unable to parse date.";
@@ -24,7 +35,6 @@ public class MovieTest {
 
     @Test
     public void getBackdropURL() {
-        Movie movie = getMovieTestObject();
         String actual = movie.getBackdropURL();
         String expected = "http://image.tmdb.org/t/p/w780/q7fXcrDPJcf6t3rzutaNwTzuKP1.jpg";
         assertEquals(expected, actual);
@@ -32,14 +42,13 @@ public class MovieTest {
 
     @Test
     public void getPosterURL() {
-        Movie movie = getMovieTestObject();
         String actual = movie.getPosterURL();
         String expected = "http://image.tmdb.org/t/p/w342/pU1ULUq8D3iRxl1fdX2lZIzdHuI.jpg";
         assertEquals(expected, actual);
     }
 
     // Helper method to generate a test object
-    private Movie getMovieTestObject() {
+    private Movie getTestObject() {
         int id = 333339;
         String title = "Ready Player One";
         String posterPath = "/pU1ULUq8D3iRxl1fdX2lZIzdHuI.jpg";
