1ca25eb5733b692d5cc08a3580631c43205f7012
Rolando Asmat <rolandoasmat@gmail.com>
2018-10-24 21:01:43 -0500

    Added new MovieDBClient using Retrofit and RX

---
+++ app/src/main/java/com/asmat/rolando/popularmovies/networking/TheMovieDBClient.kt
@@ -0,0 +1,90 @@
+package com.asmat.rolando.popularmovies.networking
+
+import com.asmat.rolando.popularmovies.BuildConfig
+import com.asmat.rolando.popularmovies.database.Movie
+import com.asmat.rolando.popularmovies.models.Cast
+import com.asmat.rolando.popularmovies.models.Review
+import com.asmat.rolando.popularmovies.models.Video
+import io.reactivex.Single
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import java.util.*
+
+object TheMovieDBClient {
+
+    //region Private
+    private const val baseURL = "https://api.themoviedb.org/3"
+
+    private val service by lazy { createService() }
+
+    /**
+     * Creates an instance of [TheMovieDBService], all requests will contain the
+     * following query parameters set: api_key, language, and region.
+     */
+    private fun createService(): TheMovieDBService {
+        val okHttpClient = OkHttpClient()
+                .newBuilder()
+                .addInterceptor { chain ->
+                    val originalRequest = chain.request()
+                    val httpURL = originalRequest.url()
+
+                    val newHttpURLBuilder = httpURL.newBuilder()
+                    newHttpURLBuilder.addQueryParameter("api_key", BuildConfig.API_KEY)
+                    newHttpURLBuilder.addQueryParameter("language", Locale.getDefault().getLanguage())
+                    newHttpURLBuilder.addQueryParameter("region", Locale.getDefault().getCountry())
+                    val newHttpURL = newHttpURLBuilder.build()
+
+                    val newRequest = originalRequest.newBuilder().url(newHttpURL).build()
+                    chain.proceed(newRequest)
+                }
+                .build()
+
+        return Retrofit.Builder()
+                .baseUrl(baseURL)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .client(okHttpClient)
+                .build()
+                .create(TheMovieDBService::class.java)
+
+    }
+    //endregion
+
+    //region API
+    fun getPopularMovies(page: Int): Single<List<Movie>> {
+        return service.getPopularMovies(page)
+    }
+
+    fun getTopRatedMovies(page: Int): Single<List<Movie>> {
+        return service.getTopRatedMovies(page)
+    }
+
+    fun getNowPlayingMovies(page: Int): Single<List<Movie>> {
+        return service.getNowPlayingMovies(page)
+    }
+
+    fun getUpcomingMovies(page: Int): Single<List<Movie>> {
+        return service.getUpcomingMovies(page)
+    }
+
+    fun getMovieDetails(movieID: String): Single<Movie> {
+        return service.getMovieDetails(movieID)
+    }
+
+    fun getMovieVideos(movieID: String): Single<List<Video>> {
+        return service.getMovieVideos(movieID)
+    }
+
+    fun getMovieReviews(movieID: String): Single<List<Review>> {
+        return service.getMovieReviews(movieID)
+    }
+
+    fun getMovieCredits(movieID: String): Single<List<Cast>> {
+        return service.getMovieCredits(movieID)
+    }
+
+    fun searchMovie(searchString: String, page: Int): Single<List<Movie>> {
+        return service.searchMovie(searchString, page)
+    }
+    //endregion
+}
--- app/src/main/java/com/asmat/rolando/popularmovies/networking/TheMovieDBService.kt
+++ app/src/main/java/com/asmat/rolando/popularmovies/networking/TheMovieDBService.kt
@@ -4,7 +4,7 @@ import com.asmat.rolando.popularmovies.database.Movie
 import com.asmat.rolando.popularmovies.models.Cast
 import com.asmat.rolando.popularmovies.models.Review
 import com.asmat.rolando.popularmovies.models.Video
-import retrofit2.Call
+import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -13,29 +13,32 @@ interface TheMovieDBService {
 
     //region movie/*
     @GET("movie/popular")
-    fun getPopularMovies(@Query("page") page: Int): Call<List<Movie>>
+    fun getPopularMovies(@Query("page") page: Int): Single<List<Movie>>
 
     @GET("movie/top_rated")
-    fun getTopRatedMovies(@Query("page") page: Int): Call<List<Movie>>
+    fun getTopRatedMovies(@Query("page") page: Int): Single<List<Movie>>
+
+    @GET("movie/now_playing")
+    fun getNowPlayingMovies(@Query("page") page: Int): Single<List<Movie>>
 
     @GET("movie/upcoming")
-    fun getUpcomingMovies(@Query("page") page: Int): Call<List<Movie>>
+    fun getUpcomingMovies(@Query("page") page: Int): Single<List<Movie>>
 
     @GET("movie/{movie_id}")
-    fun getMovieDetails(@Path("movie_id") id: String): Call<Movie>
+    fun getMovieDetails(@Path("movie_id") id: String): Single<Movie>
 
     @GET("movie/{movie_id/videos}")
-    fun getMovieVideos(@Path("movie_id") id: String): Call<List<Video>>
+    fun getMovieVideos(@Path("movie_id") id: String): Single<List<Video>>
 
     @GET("movie/{movie_id/reviews}")
-    fun getMovieReviews(@Path("movie_id") id: String): Call<List<Review>>
+    fun getMovieReviews(@Path("movie_id") id: String): Single<List<Review>>
 
     @GET("movie/{movie_id}/credits")
-    fun getMovieCredits(@Path("movie_id") id: String): Call<List<Cast>>
+    fun getMovieCredits(@Path("movie_id") id: String): Single<List<Cast>>
     //endregion
 
     //region search/*
     @GET("search/movie")
-    fun searchMovie(movieSearchStr: String): Call<List<Movie>>
+    fun searchMovie(@Query("query") searchString: String, @Query("page") page: Int): Single<List<Movie>>
     //endregion
 }
