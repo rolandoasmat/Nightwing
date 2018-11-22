d8b55368954573f5228ff042980a625599005aec
Rolando Asmat <rolandoasmat@gmail.com>
2018-11-22 14:48:39 -0600

    Updated get popular movies response classes

--- app/src/main/java/com/asmat/rolando/popularmovies/networking/models/MoviesResponse.kt
+++ app/src/main/java/com/asmat/rolando/popularmovies/networking/models/MoviesResponse.kt
@@ -1,4 +1,6 @@
 package com.asmat.rolando.popularmovies.networking.models
 
 data class MoviesResponse(val page: Int,
-                          val results: Array<MovieResponse>)
+                          val results: List<MovieResponse>,
+                          val total_results: Int,
+                          val total_pages: Int)
