a4c1ccb5f2729d58acdeed94d09961efd4f48e61
Rolando Asmat <rolandoasmat@gmail.com>
2018-11-24 23:39:10 -0600

    Converted request type and transform to Kotlin

---
+++ app/src/main/java/com/asmat/rolando/popularmovies/ui/transformations/RoundedTransformation.kt
@@ -0,0 +1,36 @@
+package com.asmat.rolando.popularmovies.ui.transformations
+
+import android.graphics.Bitmap
+import android.graphics.BitmapShader
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Shader
+
+import com.squareup.picasso.Transformation
+
+class RoundedTransformation// radius is corner radii in dp
+// margin is the board in dp
+(private val radius: Int, private val margin: Int  // dp
+) : Transformation {
+
+    override fun transform(source: Bitmap): Bitmap {
+        val paint = Paint()
+        paint.isAntiAlias = true
+        paint.shader = BitmapShader(source, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
+
+        val output = Bitmap.createBitmap(source.width, source.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(output)
+        canvas.drawRoundRect(RectF(margin.toFloat(), margin.toFloat(), (source.width - margin).toFloat(), (source.height - margin).toFloat()), radius.toFloat(), radius.toFloat(), paint)
+
+        if (source != output) {
+            source.recycle()
+        }
+
+        return output
+    }
+
+    override fun key(): String {
+        return "rounded"
+    }
+}
