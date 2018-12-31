98bb294da0ed4cc7116dc869667f7da97a83b52d
Rolando Asmat <rolandoasmat@gmail.com>
2018-12-31 08:33:51 -0600

    Made CreditsResponse parcelable

--- app/src/main/java/com/asmat/rolando/popularmovies/networking/the/movie/db/models/CreditsResponse.kt
+++ app/src/main/java/com/asmat/rolando/popularmovies/networking/the/movie/db/models/CreditsResponse.kt
@@ -1,9 +1,14 @@
 package com.asmat.rolando.popularmovies.networking.the.movie.db.models
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class CreditsResponse(val id: Int,
                            val cast: List<Cast>,
-                           val crew: List<Crew>) {
+                           val crew: List<Crew>) : Parcelable {
 
+    @Parcelize
     data class Cast(val cast_id: Int,
                     val character: String,
                     val credit_id: String,
@@ -11,13 +16,14 @@ data class CreditsResponse(val id: Int,
                     val id: Int,
                     val name: String,
                     val order: Int,
-                    val profile_path: String?)
+                    val profile_path: String?) : Parcelable
 
+    @Parcelize
     data class Crew(val credit_id: String,
                     val department: String,
                     val gender: Int?,
                     val id: Int,
                     val job: String,
                     val name: String,
-                    val profile_path: String?)
+                    val profile_path: String?) : Parcelable
 }
