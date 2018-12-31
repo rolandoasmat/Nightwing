a08eeba5f034065ee9955b72a12e0790687f9b15
Rolando Asmat <rolandoasmat@gmail.com>
2018-12-31 08:32:12 -0600

    Use literal array syntax

--- app/src/main/java/com/asmat/rolando/popularmovies/database/AppDatabase.kt
+++ app/src/main/java/com/asmat/rolando/popularmovies/database/AppDatabase.kt
@@ -5,7 +5,7 @@ import android.arch.persistence.room.*
 import com.asmat.rolando.popularmovies.database.entities.FavoriteMovie
 import com.asmat.rolando.popularmovies.database.entities.WatchLaterMovie
 
-@Database(entities = arrayOf(FavoriteMovie::class, WatchLaterMovie::class), version = 3)
+@Database(entities = [FavoriteMovie::class, WatchLaterMovie::class], version = 3)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun moviesDAO(): MoviesDAO
 }
