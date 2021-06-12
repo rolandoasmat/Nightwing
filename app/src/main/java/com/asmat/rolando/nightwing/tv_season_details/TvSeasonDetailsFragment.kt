d16ff34a3fe2b3663817c0856286852441b90600
Rolando Asmat <rolandoasmat@gmail.com>
2021-06-12 17:58:55 -0500

    Added empty fragment and viewmodel

---
+++ app/src/main/java/com/asmat/rolando/nightwing/tv_season_details/TvSeasonDetailsFragment.kt
@@ -0,0 +1,7 @@
+package com.asmat.rolando.nightwing.tv_season_details
+
+import androidx.fragment.app.Fragment
+
+class TvSeasonDetailsFragment: Fragment() {
+
+}
---
+++ app/src/main/java/com/asmat/rolando/nightwing/tv_season_details/TvSeasonDetailsViewModel.kt
@@ -0,0 +1,7 @@
+package com.asmat.rolando.nightwing.tv_season_details
+
+import androidx.lifecycle.ViewModel
+
+class TvSeasonDetailsViewModel: ViewModel() {
+
+}
