39e0a576a6fb113d681b263bc3de4ffb7e721257
Rolando Asmat <rolandoasmat@gmail.com>
2022-01-22 14:30:37 -0600

    Added tv show episode image poster

--- app/src/main/java/com/asmat/rolando/nightwing/tv_season_details/TvSeasonEpisodesAdapter.kt
+++ app/src/main/java/com/asmat/rolando/nightwing/tv_season_details/TvSeasonEpisodesAdapter.kt
@@ -3,9 +3,14 @@ package com.asmat.rolando.nightwing.tv_season_details
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.asmat.rolando.nightwing.R
+import com.asmat.rolando.nightwing.extensions.invisible
+import com.asmat.rolando.nightwing.extensions.visible
+import com.asmat.rolando.nightwing.ui.transformations.RoundedTransformation
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_row_tv_episode.view.*
 
 class TvSeasonEpisodesAdapter : RecyclerView.Adapter<TvSeasonEpisodesAdapter.ViewHolder>() {
@@ -32,10 +37,20 @@ class TvSeasonEpisodesAdapter : RecyclerView.Adapter<TvSeasonEpisodesAdapter.Vie
     override fun getItemCount() = episodes.count()
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val tvEpisodePoster: ImageView? = itemView.tvEpisodePoster
         private val episodeTitle: TextView? = itemView.episodeTitle
         private val episodeOverview: TextView? = itemView.episodeOverview
 
         fun bind(uiData: TvSeasonEpisodesUiModel.Item) {
+            if (uiData.backdropURL?.isEmpty() == true) {
+                tvEpisodePoster?.invisible()
+            } else {
+                tvEpisodePoster?.visible()
+                Picasso.get()
+                    .load(uiData.backdropURL)
+                    .transform(RoundedTransformation(50, 0))
+                    .into(tvEpisodePoster)
+            }
             episodeTitle?.text = uiData.title
             episodeOverview?.text = uiData.body
         }
